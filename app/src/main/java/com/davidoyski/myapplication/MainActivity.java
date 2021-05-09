1ca6a2581dd83a1cc80e6f98aa1234089b1dad2b
Davydouski-Dmitry <davidowski7777@gmail.com>
2021-05-09 21:43:00 +0300

    Initial commit

---
+++ app/src/main/java/com/davidoyski/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.davidoyski.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task1);
+    }
+}
